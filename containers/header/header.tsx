a5fce96792d664fda36236786ac7c6ff9cc72f1f
dmitrachkovivan <dmitrachkovivan@yandex.ru>
2023-04-09 19:44:31 +0300

    feat(gallery): добавил прогресбар в хедер

--- containers/header/header.tsx
+++ containers/header/header.tsx
@@ -1,16 +1,20 @@
 import { FC } from "react";
 import css from "./header.module.scss";
+import ProgressBar from "@/containers/header/progress-bar";
 
 export const Header: FC = () => {
   return (
     <div className={css.container}>
-      <div>
-        <a className={css.phone}>89073328743</a>
-        <span className={css.caption}>круглосуточно, без выходных</span>
+      <div className={css.wrapper}>
+        <div>
+          <a className={css.phone}>89073328743</a>
+          <span className={css.caption}>круглосуточно, без выходных</span>
+        </div>
+        <div className={css.logo}>
+          <img alt="logo" src="/eva-komi-logo.svg" />
+        </div>
       </div>
-      <div className={css.logo}>
-        <img alt="logo" src="/eva-komi-logo.svg" />
-      </div>
+      <ProgressBar />
     </div>
   );
 };

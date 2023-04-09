505da4137a17955572054842c2348e788abb7a7e
Dmitrackov_Denis <Dmitra4kovdenis@gmail.com>
2023-04-09 15:03:31 +0300

    feat(when-to-call):начало разработки блока

--- containers/page-home/when-to-call/when-to-call.tsx
+++ containers/page-home/when-to-call/when-to-call.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
-import css from "./when-to-call.module.scss"
+import css from "./when-to-call.module.scss";
 export const WhenToCall: FC = () => {
   return (
     <div className={css.container}>
       <div className={css.leftBlock}>
         <h2 className={css.title}>Когда следует вызывать эвакуатор</h2>
-        <img className={css.image} alt={"car"} src={"/when-to-call-image.png"}/>
+        <img
+          className={css.image}
+          alt={"car"}
+          src={"/when-to-call-image.png"}
+        />
       </div>
-      <div className={css.rightBlock}>
-      vsds
-      </div>
+      <div className={css.rightBlock}>vsds</div>
     </div>
-  )
-}
+  );
+};

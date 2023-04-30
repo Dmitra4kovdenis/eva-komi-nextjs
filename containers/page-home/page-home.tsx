823d829023b940319ba96e035cec8715e3b94d57
Dmitrackov_Denis <Dmitra4kovdenis@gmail.com>
2023-04-30 21:40:21 +0300

    Merge branch 'main' into случаи_вызова
    # Conflicts:
    #	containers/page-home/page-home.tsx

--- containers/page-home/page-home.tsx
+++ containers/page-home/page-home.tsx
@@ -4,15 +4,17 @@ import WhenToCall from "@/containers/page-home/when-to-call";
 import { Banner } from "@/containers/page-home/banner";
 import WorkGallery from "@/containers/page-home/work-gallery";
 import BannerRoad from "@/containers/page-home/banner-road";
+import Transportation from "@/containers/page-home/transportation";
 
 export const PageHome: FC = () => {
   return (
     <>
       <Banner />
       <Prices />
-      <WhenToCall/>
       <WorkGallery />
+      <WhenToCall />
       <BannerRoad />
+      <Transportation/>
     </>
   );
 };

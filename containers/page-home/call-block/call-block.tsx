2922da316caf12f2a2dfd68d481c89e26444e307
dmitrachkovivan <dmitrachkovivan@yandex.ru>
2023-05-28 20:08:00 +0300

    feat(header): добавил разметку и расширил цены

--- containers/page-home/call-block/call-block.tsx
+++ containers/page-home/call-block/call-block.tsx
@@ -3,14 +3,25 @@ import css from "./call-block.module.scss";
 import { phoneCompact, phoneFull } from "@/constants";
 export const CallBlock: FC = () => {
   return (
-    <div className={css.container}>
+    <div
+      className={css.container}
+      itemScope
+      itemType="http://schema.org/Organization"
+    >
+      <span itemProp="name" className={css.hidden}>
+        ЭваКоми - эвакуатор в Сыктывкаре и республике коми
+      </span>
       <div className={css.line} />
       <p className={css.text}>
         Для заказа эвакуатора, уточнения цен, акций и прочей информации, просто
         позвоните нам <br />
         Или нажмите на номер ниже, звонок начнется автоматически
       </p>
-      <a className={css.phoneNumber} href={`tel:${phoneCompact}`}>
+      <a
+        itemProp="telephone"
+        className={css.phoneNumber}
+        href={`tel:${phoneCompact}`}
+      >
         {phoneFull}
       </a>
       <p className={css.text2}>Машина приедет в течении 20 минут</p>

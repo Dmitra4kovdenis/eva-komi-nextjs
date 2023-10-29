8b65ab514e286882c127363d1c80f17ba9ed046f
Dmitrackov_Denis <Dmitra4kovdenis@gmail.com>
2023-10-29 16:29:12 +0300

    feat: добавил eslint, prettier

--- containers/page-home/banner/banner.tsx
+++ containers/page-home/banner/banner.tsx
@@ -6,30 +6,62 @@ import Image from "next/image";
 
 export const Banner: FC = () => {
   return (
-    <div className={css.banner}>
-      <div className={css.backgroundPhotoBanner} />
-      <a
-        href={`tel:${phoneCompact}`}
-        title={`Вызвать ${phoneFull}`}
-        className={css.button}
-      >
-        <IconPhone />
-      </a>
-      <Image
-        className={css.carBanner}
-        alt="Баннер с эвакуатором"
-        src="/car-banner.png"
-        width={900}
-        height={500}
-      />
-      <div className={css.content}>
-        <h1 className={css.title}>
-          Быстрая, надежная и безопасная эвакуация автомобилей!
-        </h1>
-        <div className={css.text}>
-          Эвакуация автомобилей по Сыктывкару и Республике Коми
+    <>
+      <div className={css.banner}>
+        <div className={css.backgroundPhotoBanner} />
+        <a
+          href={`tel:${phoneCompact}`}
+          title={`Вызвать ${phoneFull}`}
+          className={css.button}
+        >
+          <IconPhone />
+        </a>
+        <Image
+          className={css.carBanner}
+          alt="Баннер с эвакуатором"
+          src="/car-banner.png"
+          width={900}
+          height={500}
+        />
+        <div className={css.content}>
+          <h1 className={css.title}>
+            Быстрая, надежная и безопасная эвакуация автомобилей!
+          </h1>
+          <div className={css.text}>
+            Эвакуация автомобилей по Сыктывкару и Республике Коми
+          </div>
         </div>
       </div>
-    </div>
+      <div className={css.socialMedia}>
+        <a
+          href={"https://vk.com/evakyatorkomi"}
+          target={"_blank"}
+          className={css.vk}
+        >
+          <Image
+            className={css.vkImage}
+            alt={"Vk"}
+            width={20}
+            height={20}
+            src={"/gallery/vk.png"}
+          />
+          <div className={css.textVk}>https://vk.com/evakyatorkomi</div>
+        </a>
+        <a
+          href={"https://wa.me/79630221679"}
+          target={"_blank"}
+          className={css.vk}
+        >
+          <Image
+            className={css.vkImage}
+            alt={"Vk"}
+            width={20}
+            height={20}
+            src={"/gallery/whatsapp.svg"}
+          />
+          <div className={css.textVk}>+7 963 022-16-79</div>
+        </a>
+      </div>
+    </>
   );
 };

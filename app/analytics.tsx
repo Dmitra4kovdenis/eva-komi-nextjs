eb4987371c886fe7c0b05c26ee0f93cb5732fc89
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-03-11 18:45:18 +0300

    обновил гугл аналитику

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -20,19 +20,23 @@ export default function Analytics() {
         accurateTrackBounce:true
    });
 </script>
+
 <noscript><div><img src="https://mc.yandex.ru/watch/93501283" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->`,
         }}
       />
 
       <Script id="google-analytics" strategy="afterInteractive">
-        {`
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-HP1XWKG7JM');
-            `}
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P2FKNTZF');`}
       </Script>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2FKNTZF"
+                height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe>
+      </noscript>
     </>
   );
 }

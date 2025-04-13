4f987e1418645f9db05021ec98fffa42539fe308
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-04-13 19:19:56 +0300

    обновил номера

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
-export const phoneFull = "8 (912) 864-01-11";
-export const phoneCompact = "89128640111";
-export const phoneTitle = "8-912-864-0111";
+export const phoneFull = "+7 (912) 864-01-11";
+export const phoneCompact = "+79128640111";
+export const phoneTitle = "+7-912-864-0111";
